6b5cc7077334e025876f64fdcacd72653f7ef1ed
Ben Meyrick <bameyrick@gmail.com>
2023-07-19 17:06:08 +0100

    Change to use eslint (#43)

--- tests/sortBy.spec.ts
+++ tests/sortBy.spec.ts
@@ -150,6 +150,7 @@ describe('sortBy(prop) ', () => {
         if (typeof value === 'string') {
           return value.toLowerCase();
         } else {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
           return value;
         }
       })
