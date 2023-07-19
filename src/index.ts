8ddf8ecd6b667b5b0a7e1e21a6c5f2c8e31e0ff7
Ben Meyrick <bameyrick@gmail.com>
2023-07-19 17:02:57 +0100

    Change to use eslint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 export type SortMappingFunction = (_key: string, value: any) => any;
 
 /**
